092558192c88e105107fad38b4d3769f4ab5029a
halilatilla <atillahalil0@gmail.com>
2022-03-15 09:52:00 +0300

    Add ActiveLink

--- src/components/Menu/MenuItem/MenuItem.tsx
+++ src/components/Menu/MenuItem/MenuItem.tsx
@@ -1,11 +1,15 @@
 import { FC } from 'react'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
+import classnames from 'classnames'
 
 import IMenuItem from './MenuItem.types'
 
 const MenuItem: FC<IMenuItem> = ({ href, text }) => {
+  const { pathname } = useRouter()
+
   return (
-    <li className="capitalize hover:underline">
+    <li className={classnames('capitalize hover:text-primary', { 'text-primary': pathname === href })}>
       <Link href={href}>
         <a>{text}</a>
       </Link>
--- src/components/TournamentList/TournamentList.tsx
+++ src/components/TournamentList/TournamentList.tsx
@@ -6,7 +6,7 @@ import ITournamentList from './TournamentList.types'
 
 const TournamentList: FC<ITournamentList> = ({ tournaments, className, ...rest }) => {
   return (
-    <ul className={classnames('grid gap-8  md:grid-cols-2 xl:grid-cols-3', className)} {...rest}>
+    <ul className={classnames('grid gap-8 md:grid-cols-2 xl:grid-cols-3', className)} {...rest}>
       {tournaments.map((tournament) => (
         <TournamentCard key={tournament.id} tournament={tournament} />
       ))}
