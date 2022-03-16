d76dcd04b08fcb04fd91c0001b463daacdd758e7
halilatilla <atillahalil0@gmail.com>
2022-03-16 17:24:23 +0300

    Add animation on TournamentList

--- src/components/TournamentList/TournamentList.tsx
+++ src/components/TournamentList/TournamentList.tsx
@@ -1,19 +1,28 @@
 import { FC } from 'react'
 import classnames from 'classnames'
+import { motion } from 'framer-motion'
 
 import { useTournamentsContext } from '@src/store'
 import { getPaginatedList, getSortedList } from '@src/lib'
-import { PAGINATION_OPTIONS } from '@src/config/constants'
+import { PAGINATION_OPTIONS, TOURNAMENT_LIST_ANIMATION_OPTIONS } from '@src/config/constants'
 import TournamentCard from './TournamentCard/TournamentCard'
 import ITournamentList from './TournamentList.types'
 
 const TournamentList: FC<ITournamentList> = ({ className, page, sortBy, ...rest }) => {
   const { tournaments } = useTournamentsContext()
+
+  const spring = {
+    type: 'spring',
+    damping: 20,
+    stiffness: 200,
+  }
   return (
     <ul className={classnames('grid gap-8 md:grid-cols-2 xl:grid-cols-3', className)} {...rest}>
       {getPaginatedList(getSortedList(tournaments || [], sortBy), page, PAGINATION_OPTIONS.pageSize)?.map(
         (tournament) => (
-          <TournamentCard key={tournament?.id} tournament={tournament} />
+          <motion.li layout transition={TOURNAMENT_LIST_ANIMATION_OPTIONS} key={tournament?.id}>
+            <TournamentCard tournament={tournament} />
+          </motion.li>
         ),
       )}
     </ul>
