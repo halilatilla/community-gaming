43f81c1f8002a55914d9b227d9924722d3679e61
halilatilla <atillahalil0@gmail.com>
2022-03-18 16:32:33 +0300

    Update TournamentForm submit button

--- src/components/TournamentForm/TournamentForm.tsx
+++ src/components/TournamentForm/TournamentForm.tsx
@@ -15,20 +15,21 @@ const TournamentForm: FC<ITournamentForm> = () => {
       initialValues={addNewTournamentInitialValues}
       validationSchema={addNewTournamentValidation}
       validateOnBlur={false}
-      validateOnChange={false}
       onSubmit={onSubmitHandler}
     >
-      <Form className="mx-auto max-w-lg space-y-7">
-        <Input label="name" name="name" placeholder="Enter your name" isRequired />
-        <Input label="alias" name="alias" placeholder="Enter your alias name" isRequired />
-        <Input label="owner username" name="ownerUsername" placeholder="Enter your owner user name" isRequired />
-        <InputNumber label="prize" name="prize" placeholder="Enter your prize" isRequired />
-        <Input label="deadline" name="deadline" placeholder="Enter your deadline" isRequired type="date" />
-        <Input label="owner id" name="ownerId" disabled />
-        <FileUpload name="ownerAvatar" label="owner avatar" isRequired />
-        <FileUpload name="coverImage" label="cover image" isRequired />
-        <Button type="submit" label="Add New" className="!mt-12 block" />
-      </Form>
+      {({ dirty, isValid }) => (
+        <Form className="mx-auto max-w-lg space-y-7">
+          <Input label="name" name="name" placeholder="Enter your name" isRequired />
+          <Input label="alias" name="alias" placeholder="Enter your alias name" isRequired />
+          <Input label="owner username" name="ownerUsername" placeholder="Enter your owner user name" isRequired />
+          <InputNumber label="prize" name="prize" placeholder="Enter your prize" isRequired />
+          <Input label="deadline" name="deadline" placeholder="Enter your deadline" isRequired type="date" />
+          <FileUpload name="ownerAvatar" label="owner avatar" isRequired />
+          <Input label="owner id" name="ownerId" disabled />
+          <FileUpload name="coverImage" label="cover image" isRequired />
+          <Button type="submit" label="Add New" className="!mt-12 block" disabled={!(isValid && dirty)} />
+        </Form>
+      )}
     </Formik>
   )
 }
