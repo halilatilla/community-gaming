95285c9cf0582ec074f9f6d79d4d64acf71488fa
halilatilla <atillahalil0@gmail.com>
2022-03-20 15:10:47 +0300

    Remove unused files into lib folder

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,4 +1,2 @@
-export { default as isTextEmpty } from './isTextEmpty'
-export { default as removeWhiteSpace } from './removeWhiteSpace'
 export { default as getPaginatedList } from './getPaginatedList'
 export { default as getSortedListByVote } from './getSortedListByVote'
--- src/lib/removeWhiteSpace.ts
+++
@@ -1,3 +0,0 @@
-export default function removeWhiteSpace(text: string) {
-  return text.replace(/\s+/g, ' ').trim()
-}
